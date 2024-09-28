6aaf92a8bb00c468ad18abcf82056c08e8a2f9cb
vinodjohn <vinodjohn56@gmail.com>
2024-09-28 12:58:11 +0300

    Input component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,6 @@ export class AppComponent implements OnInit {
 
   dogTitle = 'List of dogs';
   dogsGlobal: Dog[] = [];
-  displayedColumns: string[] = ['name', 'heightInCm', 'isAlive'];
-
 
   dogData(): Dog[] {
     let dogs: Dog[] = [];
@@ -24,29 +22,7 @@ export class AppComponent implements OnInit {
     return dogs;
   }
 
-  bark(): void {
-    alert("Dogs doesn't bark always");
-  }
-
   ngOnInit(): void {
     this.dogsGlobal = this.dogData();
-  }
-
-  addRandom() {
-    this.dogsGlobal = [...this.dogsGlobal, new Dog(this.getRandomName(), this.getRandomHeight(), this.getRandomLive())];
-  }
-
-  getRandomName(): string {
-    const names = ['Rex', 'Rona', 'Milo', 'Shadow', 'Buddy'];
-    const randomIndex = Math.floor(Math.random() * names.length);
-    return names[randomIndex];
-  }
-
-  getRandomHeight(): number {
-    return Math.floor(Math.random() * 60) + 20;
-  }
-
-  getRandomLive(): boolean {
-    return Math.random() > 0.5;
   }
 }
--- src/app/dog/dog.component.ts
+++ src/app/dog/dog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Dog} from "../shared/models/Dog";
 
 @Component({
   selector: 'app-dog',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './dog.component.css'
 })
 export class DogComponent {
+  @Input() title: string = "";
+  @Input() dogs: Dog[] = [];
 
+  displayedColumns: string[] = ['name', 'heightInCm', 'isAlive'];
+
+  addRandom() {
+    this.dogs = [...this.dogs, new Dog(this.getRandomName(), this.getRandomHeight(), this.getRandomLive())];
+  }
+
+  getRandomName(): string {
+    const names = ['Rex', 'Rona', 'Milo', 'Shadow', 'Buddy'];
+    const randomIndex = Math.floor(Math.random() * names.length);
+    return names[randomIndex];
+  }
+
+  getRandomHeight(): number {
+    return Math.floor(Math.random() * 60) + 20;
+  }
+
+  getRandomLive(): boolean {
+    return Math.random() > 0.5;
+  }
+
+  bark(): void {
+    alert("Dogs doesn't bark always");
+  }
 }
